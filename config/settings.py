f6495a2c9dd4dc532a6a9fdfa8ef0941db6a26db
Your Name <your-email@example.com>
2026-08-29 15:34:05 +0300

    Configure static files

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
